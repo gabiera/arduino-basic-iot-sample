1d106c05ae5209eaa083550b1bac025dc110865d
Gabriel Abiera <gabiera@outlook.com>
2016-06-25 14:43:37 +0800

    Update demo.ino

--- demo/demo.ino
+++ demo/demo.ino
@@ -1,24 +1,6 @@
 #include <SPI.h>
 #include <Ethernet.h>
-#include <Time.h>
-
-
-IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov
-// IPAddress timeServer(132, 163, 4, 102); // time-b.timefreq.bldrdoc.gov
-// IPAddress timeServer(132, 163, 4, 103); // time-c.timefreq.bldrdoc.gov
-
-
-const int timeZone = 0;     // UTC
-//const int timeZone = -5;  // Eastern Standard Time (USA)
-//const int timeZone = -4;  // Eastern Daylight Time (USA)
-//const int timeZone = -8;  // Pacific Standard Time (USA)
-//const int timeZone = -7;  // Pacific Daylight Time (USA)
-
-
-EthernetUDP Udp;
-unsigned int udpPort = 51242;  // local port to listen for UDP packets
-
-
+//#include <Time.h>
 
 
 const int ledOutput = 7;
@@ -37,7 +19,8 @@ EthernetClient client;
 
 //char server[] = "devhost01.anetwork.local";
 //char server[] = "iot0917.azurewebsites.net";
-IPAddress server(192,168,254,89);
+//IPAddress server(192,168,254,89);
+IPAddress server(40,83,124,55);
 const int PORT = 5555;
 
 unsigned long lastConnectionTime = 0;          // last time you connected to the server, in milliseconds
@@ -53,11 +36,11 @@ void setup() {
   Serial.println("booting");
   delay(5000);
     // start the Ethernet connection:
-  //#if defined(WIZ550io_WITH_MACADDRESS)
- //   if (Ethernet.begin() == 0) {
-  //#else
- //   if (Ethernet.begin(mac) == 0) {
-  //#endif  
+  #if defined(WIZ550io_WITH_MACADDRESS)
+    if (Ethernet.begin() == 0) {
+  #else
+    if (Ethernet.begin(mac) == 0) {
+  #endif  
    //   Serial.println("Failed to configure Ethernet using DHCP");
       // no point in carrying on, so do nothing forevermore:
       // try to congifure using IP address instead of DHCP:
@@ -66,12 +49,8 @@ void setup() {
   #else
       Ethernet.begin(mac, ip,myDns,gw,snip);
   #endif  
-  //}
+  }
   Serial.println(Ethernet.localIP());
-  
-  Udp.begin(udpPort);
-  Serial.println("waiting for sync");
-  setSyncProvider(getNtpTime);
   
   registerDevice();
   client.stop();
@@ -113,11 +92,8 @@ void postState() {
         c = client.read();
       if(c == '0') {
         ledState = 0;
-        Serial.println("chicken");
-        
       } else if (c == '1') {
         ledState = 1;
-        Serial.println("curry");
       } }
       if(c == '>')
         break;
@@ -182,62 +158,7 @@ void registerDevice() {
 int getTemp() {
   int  sensorValue = analogRead(tempinput);
   float millivolts = (sensorValue/1024.0) * 5000;
-  float farenheit =  millivolts/10;
-  float celsius = (farenheit * 5.0 / 9.0) - 32;
-  return celsius;
-}
-
-
-/*-------- NTP code ----------*/
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  Serial.println("Transmit NTP Request");
-  sendNTPpacket(timeServer);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Serial.println("Receive NTP Response");
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  Serial.println("No NTP Response :-(");
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:                 
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
+  return millivolts;
 }
 
 
